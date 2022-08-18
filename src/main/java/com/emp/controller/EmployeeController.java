14221fa510c5718033cce7f07ae41f4fff0a4ba7
unknown <cogjava3123@iiht.tech>
2022-08-18 00:25:59 -0700

    changes done in  Record and employee MicroService

--- src/main/java/com/emp/controller/EmployeeController.java
+++ src/main/java/com/emp/controller/EmployeeController.java
@@ -1,10 +1,13 @@
 package com.emp.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 import com.emp.entity.Employee;
 import com.emp.service.EmployeeServiceImpl;
@@ -16,10 +19,18 @@ public class EmployeeController {
 	
 	@Autowired
 	private iEmployeeService employeeService;
-
+	
+	@Autowired
+	private RestTemplate restTemplate;
+	
 	@GetMapping("/{eid}")
 	public Employee getEmployee(@PathVariable("eid")Long eid) {
 		Employee employee = this.employeeService.getEmployee(eid);
+		
+		//http://localhost:9090/record/emp/3446\
+		List records = this.restTemplate.getForObject("http://localhost:9090/record/emp/3446" + eid,List.class);
+		employee.setRecords(records);
 		return employee;
+		
 	}
 }
